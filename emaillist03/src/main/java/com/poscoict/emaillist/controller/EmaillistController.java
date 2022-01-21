fbbd90290ab3daf63a09238522d53b1be82d2245
Bella <shdudtnr3939@naver.com>
2022-01-21 12:41:12 +0900

    22.01.21 emaillist03

--- emaillist03/src/main/java/com/poscoict/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/poscoict/emaillist/controller/EmaillistController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscoict.emaillist.repository.EmaillistRepository;
@@ -15,11 +17,23 @@ public class EmaillistController {
 	@Autowired
 	private EmaillistRepository emaillistRepository;
 
-	@ResponseBody
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
 		System.out.println(emaillistRepository);
-//		List<EmaillistVo> list = new EmaillistRepository().findAll();
-		return "emaillist";
+		List<EmaillistVo> list = new EmaillistRepository().findAll();
+		model.addAttribute("list",list);
+		
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "/WEB-INF/views/form.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
 	}
 }
