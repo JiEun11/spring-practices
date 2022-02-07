80ce915dae2171642fc11a921fa2e1ee9c422a08
Bella <shdudtnr3939@naver.com>
2022-02-07 14:18:57 +0900

    22.02.07 Java configuration 실습 2

---
+++ container/src/main/java/com/poscoict/container/config/user/AppConfig2.java
@@ -0,0 +1,15 @@
+package com.poscoict.container.config.user;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.poscoict.container.user.User;
+
+@Configuration
+public class AppConfig2 {
+
+	@Bean
+	public User user() {
+		return new User("지믕"); // 값 명시 안 해주면 default 값인 김지은이 출력된다.
+	}
+}
--- container/src/main/java/com/poscoict/container/user/test/AppConfigTest.java
+++ container/src/main/java/com/poscoict/container/user/test/AppConfigTest.java
@@ -9,17 +9,27 @@ import com.poscoict.container.user.User;
 public class AppConfigTest {
 
 	public static void main(String[] args) {
-		
+
 		testAppConfig01();
+		testAppConfig02();
 
 	}
 
+	// JAVA Config2
+	// 자바 설정 클래스가 모여 있는 베이스 패키지를 전달
+	// 설정 클래스에 @Configuration이 반드시 필요함 
+	private static void testAppConfig02() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext("com.poscoict.container.config.user");
+		
+		User user = ac.getBean(User.class);
+		System.out.println(user);
+	}
+
 	// Java Config1
-	// 직접 JAVA Class (Config Class)를 전달 
+	// 직접 JAVA Class (Config Class)를 전달
 	public static void testAppConfig01() {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig01.class);
-		
-		
+
 		User user = ac.getBean(User.class);
 		System.out.println(user);
 	}
