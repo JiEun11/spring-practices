eadb74edd87f73880ee023e9ea69de807742fcdf
Bella <shdudtnr3939@naver.com>
2022-02-08 11:21:20 +0900

    22.02.08 explicit java config #final

--- container/src/main/java/com/poscoict/container/config/videosystem/DvdPlayerConfig.java
+++ container/src/main/java/com/poscoict/container/config/videosystem/DvdPlayerConfig.java
@@ -13,12 +13,44 @@ public class DvdPlayerConfig {
 	public DigitalVideoDisc avengers() {
 		return new Avengers();
 	}
-	
+
+	/* 주입이 안 된채로 리턴한 코드...삭제하기 */
+//	@Bean
+//	public DVDPlayer dvdPlayer() {
+//		return new DVDPlayer();
+//	}
+
+	// 주입(Injection) 하기 1
+	// Bean 생성 메소드를 직접 호출하는 방법
+	// 생성자 주입 , DVDPlayer를 만드는 dvd 생성자
+	// @Bean
+	public DVDPlayer dvdPlayer01() {
+		return new DVDPlayer(avengers());
+	}
+
+	// 주입(Injection) 하기 2
+	// Parameter로 bean 전달하기
+	// 생성자 주입
+	// @Bean
+	public DVDPlayer dvdPlayer02(DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
+
+	// 주입(Injection) 하기 3
+	// Parameter로 bean 전달하기
+	// setter 주입
 	@Bean
-	public DVDPlayer dvdPlayer() {
-		return new DVDPlayer();
+	public DVDPlayer dvdPlayer03(DigitalVideoDisc dvd) {
+		DVDPlayer dvdPlayer = new DVDPlayer();
+		dvdPlayer.setDvd(dvd);
+		
+		return dvdPlayer;
 	}
 	
-	// 주입(Injection) 하기 1
-	// 
+	// 같은 타입의 bean을 생성할 경우 
+	@Bean(name="dvdPlayer04")
+	public DVDPlayer dvdPlayer04(DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
+
 }
--- container/src/main/java/com/poscoict/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/poscoict/container/videosystem/DVDPlayer.java
@@ -4,8 +4,27 @@ public class DVDPlayer {
 
 	private DigitalVideoDisc dvd;
 	
+	public DVDPlayer() {
+		
+	}
+	
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
 	public void play() {
 		dvd.play();
 	}
 
+	/* getter setter */
+	public DigitalVideoDisc getDvd() {
+		return dvd;
+	}
+
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
+	
+	
 }
